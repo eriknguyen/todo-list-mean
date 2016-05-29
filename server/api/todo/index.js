cac47d9b7708c4595c69ea180814952c49ee58a5
namkhanh164 <namkhanh@gmail.com>
2016-05-29 16:20:22 +0800

    modify Express routes, add todomvc-common

--- server/api/todo/index.js
+++ server/api/todo/index.js
@@ -5,11 +5,15 @@ var controller = require('./todo.controller');
 
 var router = express.Router();
 
-router.get('/', controller.index);
+router.$routeParams('id', controller.todo);
+
+//router.get('/', controller.index);
+router.get('/', controller.query);
 router.get('/:id', controller.show);
 router.post('/', controller.create);
 router.put('/:id', controller.update);
-router.patch('/:id', controller.update);
-router.delete('/:id', controller.destroy);
+//router.patch('/:id', controller.update);
+//router.delete('/:id', controller.destroy);
+router.delete('/:id', controller.remove);
 
 module.exports = router;
